7f5773d427f5f88aa5eb9415c443028a222ff6e7
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-10 17:27:22 -0500

    initial values needed for game

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+// guess the number game
+function guessTheNumber() {
+    console.log("Guess the number between 1 and 100");
+    let attempts = 0;
+    const targetNumber = Math.floor(Math.random() * 100) + 1;
+    let guess = null;
+}
+
+guessTheNumber();
